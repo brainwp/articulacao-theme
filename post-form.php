f2ebe0b486d7a5d1c8a1d8a463e9b9df1561788e
matheus <contato@matheusgimenez.com.br>
2015-10-06 11:11:41 -0300

    add media buttons

--- post-form.php
+++ post-form.php
@@ -14,7 +14,7 @@
 			$('#post-types a').removeClass('selected');
 			$(this).addClass('selected');
 			if ($(this).attr('id') == 'post') {
-				$('#posttitle').val("<?php echo esc_js( __('Título do Post', 'p2') ); ?>");
+				$('#posttitle').val("<?php echo esc_js( __('Post Title', 'p2') ); ?>");
 			} else {
 				$('#posttitle').val('');
 			}
@@ -26,13 +26,10 @@
 /* ]]> */
 </script>
 
-<?php $post_format = isset( $_GET['p'] ) ? $_GET['p'] : 'status'; ?>
+<?php $post_format = isset( $_GET['p'] ) ? $_GET['p'] : 'post'; ?>
 <div id="postbox">
-
-	<div class="postboxcontent">
-
 		<div class="avatar">
-			<?php echo get_avatar( get_current_user_id(), 240 ); ?>
+			<?php echo get_avatar( get_current_user_id(), 48 ); ?>
 		</div>
 
 		<div class="inputarea">
@@ -43,39 +40,37 @@
 					<?php p2_user_prompt(); ?>
 				</label>
 				<?php endif; ?>
-				<div class="inputs">
 
-					<textarea class="expand70-200" name="posttext" id="posttext" rows="4" cols="60"></textarea>
-
-					<label class="post-error" for="posttext" id="posttext_error"></label>
-					<div class="postrow">
-						<input id="posttitle" name="post_title" type="text" autocomplete="off"
-							placeholder="<?php esc_attr_e( 'Titulo', 'p2' ); ?>" />
-
-						<input id="tags" name="tags" type="text" autocomplete="off"
-							value="<?php esc_attr_e( 'Tags', 'p2' ); ?>"
-							onfocus="this.value=(this.value=='<?php echo esc_js( __( 'Tags', 'p2' ) ); ?>') ? '' : this.value;"
-							onblur="this.value=(this.value=='') ? '<?php echo esc_js( __( 'Tags', 'p2' ) ); ?>' : this.value;" />
-						<input id="submit" type="submit" value="<?php esc_attr_e( 'Postar agora', 'p2' ); ?>" />
-					</div>
-					<input type="hidden" name="post_format" id="post_format" value="0" />
-					<span class="progress spinner-post-new" id="ajaxActivity"></span>
-
-					<?php do_action( 'p2_post_form' ); ?>
-
-					<input type="hidden" name="action" value="post" />
-					<?php wp_nonce_field( 'new-post' ); ?>
+				<div id="postbox-type-post" class="post-input <?php if ( 'post' == $post_format || 'standard' == $post_format ) echo ' selected'; ?>">
+					<input type="text" name="posttitle" id="posttitle" value=""
+						onfocus="this.value=(this.value=='<?php echo esc_js( __( 'Post Title', 'p2' ) ); ?>') ? '' : this.value;"
+						onblur="this.value=(this.value=='') ? '<?php echo esc_js( __( 'Post Title', 'p2' ) ); ?>' : this.value;" />
 				</div>
+				<?php if ( current_user_can( 'upload_files' ) ): ?>
+				<div id="media-buttons" class="hide-if-no-js">
+					<?php p2_media_buttons(); ?>
+				</div>
+				<?php endif; ?>
+				<textarea class="expand70-200" name="posttext" id="posttext" rows="4" cols="60"></textarea>
+				<label class="post-error" for="posttext" id="posttext_error"></label>
+				<div class="postrow">
+					<input id="tags" name="tags" type="text" autocomplete="off"
+						value="<?php esc_attr_e( 'Tag it', 'p2' ); ?>"
+						onfocus="this.value=(this.value=='<?php echo esc_js( __( 'Tag it', 'p2' ) ); ?>') ? '' : this.value;"
+						onblur="this.value=(this.value=='') ? '<?php echo esc_js( __( 'Tag it', 'p2' ) ); ?>' : this.value;" />
+					<input id="submit" type="submit" value="<?php esc_attr_e( 'Post it', 'p2' ); ?>" />
+				</div>
+				<input type="hidden" name="post_format" id="post_format" value="post" />
+				<span class="progress spinner-post-new" id="ajaxActivity"></span>
+
+				<?php do_action( 'p2_post_form' ); ?>
+
+				<input type="hidden" name="action" value="post" />
+				<?php wp_nonce_field( 'new-post' ); ?>
 			</form>
 
 		</div>
 
 		<div class="clear"></div>
-
-		<div class="postbox-sidebar">
-			<?php dynamic_sidebar( 'beneath-post-box' ); ?>
-		</div>
-
-	</div><!--/.postboxcontent -->
 
 </div> <!-- // postbox -->
