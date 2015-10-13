058a53ea42a988babec40ad3625b7a00c32c2ed6
Diego Rojas <diego@brasa.art.br>
2015-10-13 15:59:37 -0300

    colocando novo css e post-form do master na marra

--- post-form.php
+++ post-form.php
@@ -14,7 +14,7 @@
 			$('#post-types a').removeClass('selected');
 			$(this).addClass('selected');
 			if ($(this).attr('id') == 'post') {
-				$('#posttitle').val("<?php echo esc_js( __('Post Title', 'p2') ); ?>");
+				$('#posttitle').val("<?php echo esc_js( __('Título da postagem', 'p2') ); ?>");
 			} else {
 				$('#posttitle').val('');
 			}
@@ -32,7 +32,9 @@
 			<?php echo get_avatar( get_current_user_id(), 48 ); ?>
 		</div>
 
-		<div class="inputarea">
+		<div id="poststuff" class="inputarea">
+			<?php $acf_before = sprintf('<input type="text" name="the_title" placeholder="%s">', __('Titulo', 'articulacao') );?>
+			<?php $acf_before .= '<input type="hidden" name="brasa_add_new" value="true">';?>
 
 			<form id="new_post" name="new_post" method="post" action="<?php echo site_url(); ?>/">
 				<?php if ( 'status' == $post_format || empty( $post_format ) ) : ?>
@@ -42,23 +44,29 @@
 				<?php endif; ?>
 
 				<div id="postbox-type-post" class="post-input <?php if ( 'post' == $post_format || 'standard' == $post_format ) echo ' selected'; ?>">
+				<h2>Escreva aqui!!</h2>
+				<p><strong>Título</p></strong>
 					<input type="text" name="posttitle" id="posttitle" value=""
-						onfocus="this.value=(this.value=='<?php echo esc_js( __( 'Post Title', 'p2' ) ); ?>') ? '' : this.value;"
-						onblur="this.value=(this.value=='') ? '<?php echo esc_js( __( 'Post Title', 'p2' ) ); ?>' : this.value;" />
+						onfocus="this.value=(this.value=='<?php echo esc_js( __( 'Título da postagem', 'p2' ) ); ?>') ? '' : this.value;"
+						onblur="this.value=(this.value=='') ? '<?php echo esc_js( __( 'Título da postagem', 'p2' ) ); ?>' : this.value;" />
 				</div>
 				<?php if ( current_user_can( 'upload_files' ) ): ?>
+
 				<div id="media-buttons" class="hide-if-no-js">
 					<?php p2_media_buttons(); ?>
 				</div>
+
+	<span class="media-description">Inserir Imagem</span>
+
 				<?php endif; ?>
 				<textarea class="expand70-200" name="posttext" id="posttext" rows="4" cols="60"></textarea>
 				<label class="post-error" for="posttext" id="posttext_error"></label>
 				<div class="postrow">
 					<input id="tags" name="tags" type="text" autocomplete="off"
-						value="<?php esc_attr_e( 'Tag it', 'p2' ); ?>"
-						onfocus="this.value=(this.value=='<?php echo esc_js( __( 'Tag it', 'p2' ) ); ?>') ? '' : this.value;"
-						onblur="this.value=(this.value=='') ? '<?php echo esc_js( __( 'Tag it', 'p2' ) ); ?>' : this.value;" />
-					<input id="submit" type="submit" value="<?php esc_attr_e( 'Post it', 'p2' ); ?>" />
+						value="<?php esc_attr_e( 'Tags', 'p2' ); ?>"
+						onfocus="this.value=(this.value=='<?php echo esc_js( __( 'Tags', 'p2' ) ); ?>') ? '' : this.value;"
+						onblur="this.value=(this.value=='') ? '<?php echo esc_js( __( 'Tags', 'p2' ) ); ?>' : this.value;" />
+					<input id="submit" type="submit" value="<?php esc_attr_e( 'Postar', 'p2' ); ?>" />
 				</div>
 				<input type="hidden" name="post_format" id="post_format" value="post" />
 				<span class="progress spinner-post-new" id="ajaxActivity"></span>
